cce114ddd808c6689cb65b051dea1231b96d1061
cchz <3330975656@qq.com>
2023-08-30 15:15:38 +0800

    new new

---
+++ gba-464-A5-laser-tag-data.R
@@ -0,0 +1,24 @@
+# Player Names
+red_team_players = c ( "Einstein",  "Bond",  "Spock",  "Noob",  "Snake",  "Pineapple",  "Kirk",  "Worf",  "Bean",  "Mabel" )
+green_team_players = c ( "Cupcake",  "Boss Level",  "Pudding",  "Watermelon",  "Ham Sandwich",  "Hot Sauce",  "Jelly Bean",  "Rambo",  "Weasley",  "Sherlock" )
+
+# Red Player Scores for Games 1-8
+red_player_scores_game_1 = c ( 32645, 43254, 30085, 27778, 34794, 22655, 32973, 22573, 52667, 22427 )
+red_player_scores_game_2 = c ( 44130, 40428, 20832, 34975, 36619, 36397, 31588, 42207, 14973, 21269 )
+red_player_scores_game_3 = c ( 32163, 53576, 26532, 28946, 11174, 11714, 27134, 30225, 23268, 26403 )
+red_player_scores_game_4 = c ( 21929, 49045, 38619, 25720, 22744, 53698, 30021, 37116, 24387, 47893 )
+red_player_scores_game_5 = c ( 55633, 24876, 32801, 20512, 47961, 23739, 37963, 25277, 22272, 33279 )
+red_player_scores_game_6 = c ( 30177, 19649, 31110, 15494, 33072, 42076, 41820, 31452, 33373, 42131 )
+red_player_scores_game_7 = c ( 31759, 31375, 31975, 24144, 24347, 30379, 36231, 24711, 41653, 38181 )
+red_player_scores_game_8 = c ( 35060, 15172, 42249, 17547, 12911, 17610, 19106, 29184, 29837, 28373 )
+
+# Green Player Scores for Games 1-8
+green_player_scores_game_1 = c ( 24548, 36897, 37525, 56700, 32273, 33940, 20613, 28572, 33729, 35685 )
+green_player_scores_game_2 = c ( 39750, 44572, 6245, 36579, 37148, 31459, 46677, 35618, 35141, 35239 )
+green_player_scores_game_3 = c ( 36341, 37177, 38622, 42036, 23647, 45705, 36379, 26774, 23467, 33767 )
+green_player_scores_game_4 = c ( 29791, 22841, 36018, 39865, 20988, 31962, 13360, 31564, 28691, 33899 )
+green_player_scores_game_5 = c ( 17418, 35085, 29004, 11515, 25331, 26541, 32692, 33534, 42084, 23517 )
+green_player_scores_game_6 = c ( 38350, 9773, 40169, 36308, 24686, 36011, 29035, 21147, 43224, 36988 )
+green_player_scores_game_7 = c ( 42046, 31046, 44619, 25441, 35483, 36049, 19678, 25663, 38857, 36171 )
+green_player_scores_game_8 = c ( 37927, 27397, 33541, 52990, 15259, 29571, 31181, 22372, 26975, 34130 )
+
